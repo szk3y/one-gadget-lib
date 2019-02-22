5b123e5f50220f9ece4720f3725aaf7e572d9b16
szk3y <d4tt423@gmail.com>
2019-02-22 19:10:29 +0900

    Increment the version number

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ install_requires = [
 
 setuptools.setup(
     name='one_gadget',
-    version='1.0.6',
+    version='1.1.0',
     author='szk3y',
     install_requires=install_requires,
     author_email='d4tt423@gmail.com',
